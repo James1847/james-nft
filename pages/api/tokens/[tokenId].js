b5979a07a368da26163616a6c7c466380b361cd9
James <1536755789@qq.com>
2021-11-05 14:54:50 +0800

    return token

--- pages/api/tokens/[tokenId].js
+++ pages/api/tokens/[tokenId].js
@@ -1,6 +1,15 @@
+import tokens from "../../../public/tokens.json"
+
+// const tokens = require('../../../public/tokens.json')
+
 export default function requestHandler(req, res) {
-  
-  res.status(200).json({ name: "John Doe" });
+  var tokenId = req.query.tokenId
+  const token = tokens[tokenId]
+  if(typeof token === 'undefined') {
+    return res.status(500).json({code: 500, message: "no such token"})
+  } else {
+    return res.status(200).json({code: 200, message: "success", data: {tokenId: tokenId, data: token}})
+  }
 }
 
 
