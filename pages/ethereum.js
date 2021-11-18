894ec2760619a7a74a0bc6e417bc72f36f9833f7
James <1536755789@qq.com>
2021-11-18 23:53:53 +0800

    complete demo

--- pages/ethereum.js
+++ pages/ethereum.js
@@ -8,10 +8,10 @@ const getBlockchain = () =>
         await window.ethereum.enable();
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
-        console.log('networkversion:', window.ethereum.networkVersion)
+        console.log('networkversion:', NFT.networks["4"])
 
         const nft = new Contract(
-          NFT.networks[window.ethereum.network].address,
+          NFT.networks["4"].address,
           NFT.abi,
           signer
         );
