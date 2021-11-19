395d85d3750f17379c97cb7d6988ea91e32c8776
James <1536755789@qq.com>
2021-11-19 10:22:43 +0800

    change proxy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 // next.config.js
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://james-nft.herokuapp.com/:path*',
-      },
-    ]
-  },
   reactStrictMode: true,
 };
